323ad0bf7a0dc22fa43e1cfc75a1f8043b6e8bb1
Thony <161661760+TonhyG@users.noreply.github.com>
2025-08-21 20:41:16 -0300

    Operadores Lógicos, Relacionais e Condicional

---
+++ Função IF/OperadoresLogicos.py
@@ -0,0 +1,31 @@
+"""
+Operadores relacionais em Python
+and conjunção (e)
+or disjunção (ou)
+not negação (não)
+"""
+
+numero1 = 10
+numero2 = 20
+numero3 = 30
+numero4 = 40
+
+if numero1 < numero2 and numero3 < numero4:
+    print("As duas condições são verdadeiras.")
+
+if numero1 < numero2 or numero3 > numero4:
+    print("Pelo menos uma das condições é verdadeira.")
+
+if not numero1 > numero2:
+    print("A condição é falsa, então a negação é verdadeira.")
+
+print("\n\n")
+
+nota = input("Digite a nota do aluno: ")
+
+if float(nota) >= 7:
+    print("Aprovado")
+if float(nota) >= 5:
+    print("Recuperação")
+else:
+    print("Reprovado")
---
+++ Função IF/OperadoresRelacionais.py
@@ -0,0 +1,31 @@
+"""
+Operadores relacionais em Python
+== igualdade
+!= diferença
+> maior que
+< menor que
+>= maior ou igual
+<= menor ou igual
+"""
+
+numero1 = 10
+numero2 = 20
+
+if numero1 == numero2:
+    print("Os números são iguais.")
+
+if numero1 != numero2:
+    print("Os números são diferentes.")
+
+if numero1 > numero2:
+    print("O número 1 é maior que o número 2.")
+
+if numero1 < numero2:
+    print("O número 1 é menor que o número 2.")
+
+if numero1 >= numero2:
+    print("O número 1 é maior ou igual ao número 2.")
+
+if numero1 <= numero2:
+    print("O número 1 é menor ou igual ao número 2.")
+    
